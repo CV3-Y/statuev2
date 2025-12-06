8b31f497c1bf9d1209036a2ae0ff8154148e9625
EYY <civan3100@gmail.com>
2025-12-07 07:51:57 +0900

    Update index.tsx

--- my-status-board/src/index.tsx
+++ my-status-board/src/index.tsx
@@ -46,7 +46,7 @@ app.get('/', async (c) => {
 
       {/* === 상단 정보 === */}
       <div style={{ position: 'absolute', top: 225, left: 120, fontSize: 60, fontWeight: 600 }}>Date</div>
-      <div style={{ position: 'absolute', top: 330, left: 75, width: 200, display: 'flex', justifyContent: 'center', fontSize: 32, fontWeight: 400, color: '#ffffff' }}>{date || 'N일차'}</div>
+      <div style={{ position: 'absolute', top: 345, left: 55, width: 200, display: 'flex', justifyContent: 'center', fontSize: 32, fontWeight: 400, color: '#ffffff' }}>{date || 'N일차'}</div>
 
       <div style={{ position: 'absolute', top: 150, left: 672, fontSize: 48, fontWeight: 600 }}>Time</div>
       <div style={{ position: 'absolute', top: 260, left: 640, width: 200, display: 'flex', justifyContent: 'center', fontSize: 32, fontWeight: 400, color: '#ffffff' }}>{time || 'HH:MM'}</div>
